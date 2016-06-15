183cc001b30b93fd7b28beaaf45920d22d16ad47
LKA-Admin <design2@anim8.com>
2016-06-15 13:58:39 -0700

    Add ArrayAdapter - makes listView actually show up!

--- app/src/main/java/com/anim8/edu/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/anim8/edu/sunshine/MainActivityFragment.java
@@ -5,11 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    //ListView list;
 
     public MainActivityFragment() {
     }
@@ -29,9 +37,21 @@ public class MainActivityFragment extends Fragment {
             "Sat 6/28 - TRAPPED IN WEATHER STATION - 23/18",
             "Sun 6/29 - Sunny - 20/7"
         };
-//        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));/*
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        return rootView;
+
+        View fragmentView = inflater.inflate(R.layout.fragment_main, container, false);
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        // This code was missing. Withotu it, listview blank - found online
+        // at http://stackoverflow.com/questions/30045343/listview-not-showing-up-in-my-layout-android-studios
+        ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+        ListView listView = (ListView) fragmentView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return fragmentView;
 
         //NOTE:  This was original code
 //        return inflater.inflate(R.layout.fragment_main, container, false);
