3cc53dabf1a42389c454476194ef92f74c49a77b
LKA-Admin <design2@anim8.com>
2016-06-21 20:21:23 -0700

    Add some comments, working, end of Lesson 1 (Layout)

--- app/src/main/java/com/anim8/edu/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/anim8/edu/sunshine/MainActivityFragment.java
@@ -17,7 +17,7 @@ import java.util.List;
  */
 public class MainActivityFragment extends Fragment {
 
-    //ArrayList
+    //Define string ArrayAdapter variable
     ArrayAdapter<String> mForecastAdapter;
 
     public MainActivityFragment() {
@@ -38,28 +38,23 @@ public class MainActivityFragment extends Fragment {
             "Sat 6/28 - TRAPPED IN WEATHER STATION - 23/18",
             "Sun 6/29 - Sunny - 20/7"
         };
+        //create string ArrayList from data
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
 
-        // This code was missing. Without it, listview blank - found online
-        // at http://stackoverflow.com/questions/30045343/listview-not-showing-up-in-my-layout-android-studios
-        //tweaked in later videos
-
-        //create an adapter
+        //create an adapter and assign to ArrayAdapter variable
         mForecastAdapter = new ArrayAdapter<String>(
+                //parameters
                 getActivity(), //context (current activity)
-                R.layout.list_item_forecast, //ID of list item layout
-                R.id.list_item_forecast_textview, //ID of text view
+                R.layout.list_item_forecast, //ID of list item layout XML file
+                R.id.list_item_forecast_textview, //ID of text view element
                 weekForecast); //Array List of data
 
         //create fragment view
-        View fragmentView =inflater.inflate(R.layout.fragment_main,container, false);
-        //create list view & populate it:
+        View fragmentView = inflater.inflate(R.layout.fragment_main,container, false);
+        //find list view & populate it:  fragmentView prefix limits search scope
         ListView listView = (ListView) fragmentView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
         return fragmentView;
-
-        //NOTE:  This was original code
-//        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
