6c647f19b832e37ee07875c2caae7724d29b02cf
LKA-Admin <design2@anim8.com>
2016-06-23 17:51:54 -0700

    Add Network connection AsyncTask stub & fragment refresh button (for debug)

---
+++ app/src/main/java/com/anim8/edu/sunshine/ForecastFragment.java
@@ -0,0 +1,175 @@
+package com.anim8.edu.sunshine;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+
+    //public static final String TAG = "ForecastFragment";
+    //Define string ArrayAdapter variable
+    ArrayAdapter<String> mForecastAdapter;
+
+    public ForecastFragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true); //Must override onCreate to tell fragment it has OptionsMenu
+    }
+
+    @Override //Then we override onCreateOptions menu to inflate our forecastfragment XML
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.forecastfragment, menu);  //we created forecastfragment
+    }
+
+    @Override //we want to be notified when an item is selected so override:
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh){
+            //following lines for debug only (as is this whole 'Refresh button' itself!)
+            Toast.makeText(getActivity(),"Refresh button selected",Toast.LENGTH_LONG).show();
+            return true; //debug temp return value only
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //create some dummy data for the ListView Here's a sample weekly forecast
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thur 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHER STATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        //create string ArrayList from data
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+        //create ArrayAdapter variable & assign it data from a source (like dummy data)
+        mForecastAdapter = new ArrayAdapter<String>(
+                //parameters
+                getActivity(), //context (current activity)
+                R.layout.list_item_forecast, //ID of list item layout XML file
+                R.id.list_item_forecast_textview, //ID of text view element
+                weekForecast); //Array List of data
+
+        //create fragment view
+        View fragmentView = inflater.inflate(R.layout.fragment_main, container, false);
+        //find list view & populate it:  fragmentView prefix limits search scope
+        ListView listView = (ListView) fragmentView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+        return fragmentView;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+        // try to open a network connection (using Github snippet from
+        // https://gist.github.com/anonymous/1c04bf2423579e9d2dcd
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are available at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                //URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7";
+                String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+                URL url = new URL(baseUrl.concat(apiKey));
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+
+
+            return null;
+        }
+
+    } //closing bracket for FetchWeatherTask
+}
